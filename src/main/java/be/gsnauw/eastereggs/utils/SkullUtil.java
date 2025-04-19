df4482a9b7fc5ccee3d8a35035bf3ff29a4abb94
Gsnauw <gillessnauwaert@gmail.com>
2025-04-19 03:53:01 +0200

    Error with using base64 textures on 1.20+

--- src/main/java/be/gsnauw/eastereggs/utils/SkullUtil.java
+++ src/main/java/be/gsnauw/eastereggs/utils/SkullUtil.java
@@ -1,8 +1,11 @@
 package be.gsnauw.eastereggs.utils;
 
+import com.destroystokyo.paper.profile.PlayerProfile;
+import com.destroystokyo.paper.profile.ProfileProperty;
 import com.mojang.authlib.GameProfile;
 import com.mojang.authlib.properties.Property;
 import lombok.Getter;
+import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
@@ -16,6 +19,23 @@ public class SkullUtil {
     private static final SkullUtil instance = new SkullUtil();
 
     public void placeCustomHead(Location location, String base64Texture) {
+        Block block = location.getBlock();
+        block.setType(Material.PLAYER_HEAD);
+
+        Skull skull = (Skull) block.getState();
+
+        // Maak een custom PlayerProfile aan
+        PlayerProfile profile = Bukkit.createProfile(UUID.randomUUID(), "EasterEgg");
+
+        // Voeg base64 texture toe
+        profile.setProperty(new ProfileProperty("textures", base64Texture));
+
+        // Zet het profiel in de Skull
+        skull.setPlayerProfile(profile);
+        skull.update();
+    }
+
+    public void placeCustomHeads(Location location, String base64Texture) {
         Block block = location.getBlock();
         block.setType(Material.PLAYER_HEAD);
 
